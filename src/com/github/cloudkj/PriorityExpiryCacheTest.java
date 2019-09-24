fbdfd4274a76d4907fc0ac10f3ca9d7221708a87
cloudkj <cloudkj@users.noreply.github.com>
2019-09-23 23:55:40 -0700

    Add test cases

--- src/com/github/cloudkj/PriorityExpiryCacheTest.java
+++ src/com/github/cloudkj/PriorityExpiryCacheTest.java
@@ -1,31 +1,118 @@
 package com.github.cloudkj;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class PriorityExpiryCacheTest {
 
     public static void main(String[] args) throws InterruptedException {
-        PriorityExpiryCache<String, Integer> cache = new PriorityExpiryCache<>(5);
-        cache.set("A", 1, 5, 100);
-        cache.set("B", 2, 15, 3);
-        cache.set("C", 3, 5, 10);
-        cache.set("D", 4, 1, 15);
-        cache.set("E", 5, 5, 150);
+        testExpiration();
+        testPriority();
+        testLRU();
+    }
 
+    public static void test() throws InterruptedException {
+        final PriorityExpiryCache<String, Integer> cache = new PriorityExpiryCache<>(5);
+        cache.set("A", 1, 5, 1000);
+        cache.set("B", 2, 15, 30);
+        cache.set("C", 3, 5, 100);
+        cache.set("D", 4, 1, 150);
+        cache.set("E", 5, 5, 1500);
         cache.get("C");
-        cache.setMaxItems(5);
-        System.out.println(cache.keys());
 
-        Thread.sleep(5);
+        cache.setMaxItems(5);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "B", "C", "D", "E")));
+
+        Thread.sleep(50);
 
         cache.setMaxItems(4);
-        System.out.println(cache.keys());
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "C", "D", "E")));
 
         cache.setMaxItems(3);
-        System.out.println(cache.keys());
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "C", "E")));
 
         cache.setMaxItems(2);
-        System.out.println(cache.keys());
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "E")));
 
         cache.setMaxItems(1);
-        System.out.println(cache.keys());
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C")));
+    }
+
+    public static void testExpiration() throws InterruptedException {
+        final PriorityExpiryCache<String, Integer> cache = new PriorityExpiryCache<>(5);
+        cache.set("A", 1, 0, 100);
+        cache.set("B", 2, 0, 0);
+        cache.set("C", 3, 0, 1000);
+        cache.set("D", 4, 0, 500);
+        cache.set("E", 5, 0, 250);
+
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "B", "C", "D", "E")));
+
+        Thread.sleep(50);
+        cache.setMaxItems(4);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "C", "D", "E")));
+
+        Thread.sleep(100);
+        cache.setMaxItems(3);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D", "E")));
+
+        Thread.sleep(200);
+        cache.setMaxItems(2);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D")));
+
+        Thread.sleep(250);
+        cache.setMaxItems(1);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C")));
+    }
+
+    public static void testPriority() {
+        final PriorityExpiryCache<String, Integer> cache = new PriorityExpiryCache<>(5);
+        cache.set("A", 1, 2, 100);
+        cache.set("B", 2, 1, 100);
+        cache.set("C", 3, 5, 100);
+        cache.set("D", 4, 4, 100);
+        cache.set("E", 5, 3, 100);
+
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "B", "C", "D", "E")));
+
+        cache.setMaxItems(4);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "C", "D", "E")));
+
+        cache.setMaxItems(3);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D", "E")));
+
+        cache.setMaxItems(2);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D")));
+
+        cache.setMaxItems(1);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C")));
+    }
+
+    public static void testLRU() {
+        final PriorityExpiryCache<String, Integer> cache = new PriorityExpiryCache<>(5);
+        cache.set("A", 1, 0, 100);
+        cache.set("B", 2, 0, 100);
+        cache.set("C", 3, 0, 100);
+        cache.set("D", 4, 0, 100);
+        cache.set("E", 5, 0, 100);
+        cache.get("B");
+        cache.get("A");
+        cache.get("E");
+        cache.get("D");
+        cache.get("C");
+
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "B", "C", "D", "E")));
+
+        cache.setMaxItems(4);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("A", "C", "D", "E")));
+
+        cache.setMaxItems(3);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D", "E")));
+
+        cache.setMaxItems(2);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C", "D")));
+
+        cache.setMaxItems(1);
+        assert cache.keys().equals(new HashSet<>(Arrays.asList("C")));
     }
 }
